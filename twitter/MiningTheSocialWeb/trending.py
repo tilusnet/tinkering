ccbb0d71f77d58d6e2c81efebfc2ff9a08222aca
Attila Szasz <tilusnet@gmail.com>
2013-05-19 21:47:07 +0100

    Improvements.

--- twitter/MiningTheSocialWeb/trending.py
+++ twitter/MiningTheSocialWeb/trending.py
@@ -3,15 +3,21 @@ import twitter
 import json
 import os
 
-consumer_key = '0gWw5SAxSBOrKziBM2cQ'
-consumer_secret = '736IKboVchX9XTOMPBSQ9tySRY4rmTmZCva9KM8OQNQ'
 
-tilusnet01_creds = os.path.expanduser('~/.twitter/tilusnet01_access')
-if not os.path.exists(tilusnet01_creds):
+def read_token_file(filename):
+    f = open(filename)
+    return f.readline().strip(), f.readline().strip()
+
+
+tilusnet01_consumer_creds = os.path.expanduser('~/.twitter/tilusnet01_consumer_creds')
+tilusnet01_token_creds = os.path.expanduser('~/.twitter/tilusnet01_token_creds')
+consumer_key, consumer_secret = read_token_file(tilusnet01_consumer_creds)
+
+if not os.path.exists(tilusnet01_token_creds):
     twitter.oauth_dance("tilusnet-01", consumer_key, consumer_secret,
-                tilusnet01_creds)
+                tilusnet01_token_creds)
 
-oauth_token, oauth_secret = twitter.oauth.read_token_file(tilusnet01_creds)
+oauth_token, oauth_secret = twitter.oauth.read_token_file(tilusnet01_token_creds)
 
 twitter_api = twitter.Twitter(auth=twitter.oauth.OAuth(
     oauth_token, oauth_secret, consumer_key, consumer_secret))
@@ -23,6 +29,7 @@ woe_usa = 23424977
 woe_uk = 23424975
 woe_ita = 23424853
 woe_fra = 23424819
+woe_tm = 881802
 
-trends = twitter_api.trends.place(_id=woe_ita)
+trends = twitter_api.trends.place(_id=woe_fra)
 print json.dumps(trends)
